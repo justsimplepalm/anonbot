f49ad5761fa4ee75f80f80dbf4464723d7f749d5
justsimplepalm <38886898+justsimplepalm@users.noreply.github.com>
2021-10-30 00:01:49 +0400

    Update db_funcs.py

--- db/db_funcs.py
+++ db/db_funcs.py
@@ -6,63 +6,60 @@ sql: Cursor = db.cursor()
 
 
 def new_user(tg_id, username, sex):
-    sql.execute(f"""INSERT INTO 'users' (tg_id, username, sex) VALUES ('{tg_id}', '{username}', '{sex}')""")
+    sql.execute(f"INSERT INTO 'users' ('tg_id', 'username', 'sex') VALUES ('{tg_id}', '{username}', '{sex}')")
     db.commit()
 
 
-def add_to_queue(tg_id, sex):
-    sql.execute(f"""INSERT INTO 'queue' (tg_id, sex) VALUES ('{tg_id}', '{sex}')""")
-    db.commit()
+def user_exists(tg_id):
+    user_exi = sql.execute(f"SELECT id FROM 'users' WHERE tg_id = {tg_id}").fetchone()
+    return bool(user_exi)
 
 
-def remove_from_queue(tg_id):
-    sql.execute(f"""DELETE FROM 'queue' WHERE tg_id = '{tg_id}'""")
-    db.commit()
-
-
-def search(sex=None):
-    if sex is None:
-        user = sql.execute(f"""SELECT tg_id FROM 'queue'""").fetchone()
-    else:
-        user = sql.execute(f"""SELECT tg_id FROM 'queue' WHERE sex = {sex}""").fetchone()
-    db.commit()
-    return user
-
-
-def queue_exists(tg_id):
-    user_exist = sql.execute(f"""SELECT tg_id FROM 'queue' WHERE tg_id = {tg_id}""").fetchall()
-    return bool(len(user_exist))
-
-
-def users_exists(tg_id):
-    user_exist = sql.execute(f"""SELECT tg_id FROM 'users' WHERE tg_id = {tg_id}""").fetchall()
-    return bool(len(user_exist))
-
-
-def update_connection(tg_id, conn_with):
-    sql.execute(f"""UPDATE 'users' SET conn_with = {conn_with} WHERE tg_id = {tg_id}""")
-    db.commit()
-
-
-def select_conn_with_self(tg_id):
-    conn_with_self = sql.execute(f"""SELECT tg_id FROM 'users' WHERE conn_with = {tg_id}""").fetchall()
-    db.commit()
-    return conn_with_self
+def check_queue(tg_id):
+    user_in_search = sql.execute(f"SELECT in_search FROM 'users' WHERE tg_id = {tg_id}").fetchone()
+    return user_in_search[0]
 
 
 def select_conn_with(tg_id):
-    conn_with = sql.execute(f"""SELECT conn_with FROM 'users' WHERE tg_id = {tg_id}""").fetchall()
-    db.commit()
+    conn_with = sql.execute(f"SELECT conn_with FROM 'users' WHERE tg_id = {tg_id}").fetchone()
     return conn_with
 
 
-def get_user_sex(tg_id):
-    sex = sql.execute(f"""SELECT sex FROM 'users' WHERE tg_id = {tg_id}""").fetchone()
-    db.commit()
-    return sex
+def select_conn_with_self(tg_id):
+    conn_with_self = sql.execute(f"SELECT tg_id FROM 'users' WHERE conn_with = {tg_id}").fetchone()
+    return conn_with_self
 
 
-def count_users():
-    count = sql.execute("SELECT COUNT(*) FROM 'users'").fetchone()
+def exit_queue(tg_id):
+    sql.execute(f"UPDATE 'users' SET in_search = 'False' WHERE tg_id = {tg_id}")
+    sql.execute(f"UPDATE 'users' SET sex_to_search = 'none' WHERE tg_id = {tg_id}")
     db.commit()
-    return count[0]
+
+
+def update_conn_with(tg_id, conn_with):
+    sql.execute(f"UPDATE 'users' SET conn_with = {conn_with} WHERE tg_id = {tg_id}")
+    db.commit()
+
+
+def update_count(tg_id):
+    sql.execute(f"UPDATE 'users' SET all_message = all_message + 1 WHERE tg_id = {tg_id}")
+    db.commit()
+
+
+def add_to_queue(tg_id, sex_to_search=None, in_search=True):
+    sql.execute(f"UPDATE 'users' SET sex_to_search = '{sex_to_search}' WHERE tg_id = {tg_id}")
+    sql.execute(f"UPDATE 'users' SET in_search = '{in_search}' WHERE tg_id = {tg_id}")
+    db.commit()
+
+
+def search():
+    search_result = sql.execute("SELECT tg_id FROM 'users' WHERE in_search = 'True'").fetchone()
+    try:
+        return search_result[0]
+    except TypeError:
+        pass
+
+
+def count():
+    count_users = sql.execute("SELECT COUNT(*) FROM 'users'").fetchone()
+    return count_users[0]
